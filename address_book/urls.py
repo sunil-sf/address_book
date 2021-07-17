6b69b9a18ad8a69f3e47574b7ae9f0fc5ec814ef
sunil <sunil@solframe.in>
2021-07-17 10:29:13 +0530

    initial commit

---
+++ address_book/urls.py
@@ -0,0 +1,9 @@
+from django .urls import path
+from .views import *
+from .import views
+app_name='address_book'
+
+urlpatterns = [
+    path('',views.home, name='home'),
+    path('add_address/',views.add_address, name='add_address'),
+]
---
+++ address_book/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render,redirect
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html',{})
+
+
+def add_address(request):
+    return render(request,'add_address.html',{})
